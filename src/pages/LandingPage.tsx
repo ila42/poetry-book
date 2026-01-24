7eed22d2442d64ba5172c7ac8515606905465ed8
ila42 <ila42lol@yandex.com>
2026-01-24 22:49:50 +0300

    изменил кнопку

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -134,12 +134,12 @@ export function LandingPage() {
               </p>
               
               <div className="flex flex-col sm:flex-row gap-4 justify-center lg:justify-start">
-                {/* Кнопка "Читать книгу" - единый стиль */}
+                {/* Кнопка "Читать" - единый стиль */}
                 <Link
                   to="/read"
                   className="btn-cta"
                 >
-                  <span>Читать книгу</span>
+                  <span>Читать</span>
                   <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z" />
                   </svg>
